12ad8f46355f53d5358194e63afd53a701da412c
alizare1 <ali.zare2000123@gmail.com>
2019-05-17 17:35:48 +0430

    add Publisher.cpp

---
+++ publisher.cpp
@@ -0,0 +1,39 @@
+#include "publisher.h"
+
+using namespace std;
+
+Publisher::Publisher(SignupArgs args, int _id) 
+    :User(args, _id) {}
+
+void Publisher::addNewFilm(Film* film) {
+    producedFilms.push_back(film);
+    for (int i = 0; i < followers.size(); i++) {
+        followers[i]->addNotif(PUBLISHER + ' ' + username + 
+            WITH_ID + to_string(id) + REGISTER_NEW_FILM);
+    }
+}
+
+void Publisher::showFollowers() {
+    cout << LIST_OF_FOLLOWERS << endl 
+    <<  FOLLOWERS_LIST_HEADER << endl;
+    for (int i = 0; i < followers.size(); i++) {
+        cout << i + 1 << "." << followers[i] << endl;
+    }
+}
+
+void Publisher::showFilms(SearchFilmsArgs args) {
+    cout << GET_FILMS_HEADER << endl;
+    int num = 1;
+    for (int i = 0; i < producedFilms.size(); i++) {
+        if (producedFilms[i]->isInFilter(args))
+            cout << num++ << ". " << producedFilms[i] << endl;
+    }
+}
+
+void Publisher::addFollower(User* user) {
+    for (int i = 0; i < followers.size(); i++) {
+        if (user == followers[i])
+            return;
+    }
+    followers.push_back(user);
+}
