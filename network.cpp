ebda23346cf58d1b6a2b2d971b94782e36ffe8ce
alizare1 <ali.zare2000123@gmail.com>
2019-05-17 18:16:06 +0430

    define login method

--- network.cpp
+++ network.cpp
@@ -5,9 +5,11 @@ using namespace std;
 void Network::signup(SignupArgs& args) {
     if (usersByName.count(args.username))
         throw BadRequest();
+    currPub = nullptr;
+    currUser = nullptr;
     User* newUser;
     if (args.publisher) {
-        Publisher* newPub = new Publisher(args, users.size() + 1);
+        Publisher* newPub = new Publisher(args, usersById.size() + 1);
         publishers[newPub->getId()] = newPub;
         newUser = newPub;
         currPub = newPub;
@@ -19,3 +21,16 @@ void Network::signup(SignupArgs& args) {
     currUser = newUser;
 }
 
+void Network::login(LoginArgs& args) {
+    if (!usersByName.count(args.username))
+        throw BadRequest();
+    if (usersByName[args.username]->checkPassword(args.password)) {
+        currUser = usersByName[args.username];
+        if (currUser->isPublisher())
+            currPub = publishers[currUser->getId()];
+        else
+            currPub = nullptr;
+    }
+    else 
+        throw BadRequest();
+}
