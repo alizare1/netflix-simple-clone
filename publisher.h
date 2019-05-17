994dd78c68535e8ff5530ab8aa7a8e64e073bddc
alizare1 <ali.zare2000123@gmail.com>
2019-05-17 17:36:33 +0430

    add forward declaration

--- publisher.h
+++ publisher.h
@@ -2,15 +2,26 @@
 #define PUBLISHER_H
 
 #include "user.h"
+#include <string>
+#include "film.h"
+#include <iostream>
+#include <vector>
+#include "structs.h"
+#include "defines.h"
+
+class Film;
+class User;
 
 class Publisher : public User {
 public:
     Publisher(SignupArgs args, int _id);
-    void addNewFilm();
+    void addNewFilm(Film* film);
     void showFollowers();
-    void showMovies(SearchFilmsArgs args);
+    void showFilms(SearchFilmsArgs args);
+    void addFollower(User* user);
     
 private:
+    
     std::vector<Film*> producedFilms;
     std::vector<User*> followers;
 
